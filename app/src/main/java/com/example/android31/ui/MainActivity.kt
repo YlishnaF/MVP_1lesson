625f931fca3b5af82678a5f18fae4732de4be34a
YlishnaF <ylishnaf@yandex.ru>
2021-03-05 10:07:30 +0300

    first commit

--- app/src/main/java/com/example/android31/ui/MainActivity.kt
+++ app/src/main/java/com/example/android31/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.android31.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.android31.R
 import com.example.android31.databinding.ActivityMainBinding
 import com.example.android31.mvp.presenter.MainPresentor
 import com.example.android31.mvp.view.MainView
@@ -19,20 +18,31 @@ class MainActivity : AppCompatActivity(), MainView {
         vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb?.root)
 
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
+        val listener1 = View.OnClickListener {
+            presenter.counterClickFirst(it.id)
+        }
+        val listener2 = View.OnClickListener {
+            presenter.counterClickSecond(it.id)
+        }
+        val listener3 = View.OnClickListener {
+            presenter.counterClickThird(it.id)
         }
 
-        vb?.btnCounter1?.setOnClickListener(listener)
-        vb?.btnCounter2?.setOnClickListener(listener)
-        vb?.btnCounter3?.setOnClickListener(listener)
+        vb?.btnCounter1?.setOnClickListener(listener1)
+        vb?.btnCounter2?.setOnClickListener(listener2)
+        vb?.btnCounter3?.setOnClickListener(listener3)
     }
 
-    override fun setButtonText(index: Int, text: String) {
-        when(index){
-            0 -> vb?.btnCounter1?.text = text
-            1 -> vb?.btnCounter2?.text = text
-            2 -> vb?.btnCounter3?.text = text
-        }
+    override fun setButtonTextFirstButton(index: Int, text: String) {
+        vb?.btnCounter1?.text = text
     }
+
+    override fun setButtonTextSecondButton(index: Int, text: String) {
+        vb?.btnCounter2?.text = text
+    }
+
+    override fun setButtonTextThirdButton(index: Int, text: String) {
+        vb?.btnCounter3?.text = text
+    }
+
 }
